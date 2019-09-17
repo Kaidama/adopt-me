37007d8f3edd4a339b3cebd1fc5c5d74f64f5cc3
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-09-17 18:10:21 -0400

    added reach router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@
 // const Pet = (props) => { React.createElement("h1", {}, name ) }
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import { Details } from "./Details";
+//reach router will choose the most specific route, react-router will render both specific and non specific routes
+
+
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to='/'>Adopt Me! </Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
